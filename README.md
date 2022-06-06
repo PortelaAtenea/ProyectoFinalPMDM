3b0012c41b1ac5f7e7976c7c9cae99f99b153bbb
a20ateneapf <portelafernandezatenea@gmail.com>
2022-06-06 13:23:19 +0200

    Comit

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 # ProyectoFinalPMDM
+https://codecanyon.net/item/city-guide-map-app-for-android/12026931?_ga=2.23108538.181393398.1654511258-756129131.1654511258
--- app/src/main/java/com/example/proyectofinalateneaportela/MainActivity.java
+++ app/src/main/java/com/example/proyectofinalateneaportela/MainActivity.java
@@ -2,13 +2,62 @@ package com.example.proyectofinalateneaportela;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.view.accessibility.AccessibilityManager;
+import android.widget.AdapterView;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageButton;
+import android.widget.LinearLayout;
+import android.widget.ListView;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
+    private Button btnCalcular;
+    private Button btnSelectCity;
+    private LinearLayout LlBottom;
+    private LinearLayout LLPortada;
+    private ListView lvCities;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        inicializar_datos();
+        //Evento que gestiona el click largo sobre el Texto Bienvenido/ Welcome
+        btnCalcular.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                LlBottom.setVisibility(View.VISIBLE);
+                LLPortada.setVisibility(View.GONE);
+            }
+        });
+        lvCities.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
+                String city = (String) adapterView.getItemAtPosition(i);
+                Toast.makeText(MainActivity.this, "Ciudad Elegida: "+city, Toast.LENGTH_SHORT).show();
+            }
+        });
     }
+    public void onClickbtn(View view) {
+        Toast.makeText(this, "bonton son funcionalidad", Toast.LENGTH_SHORT).show();
+
+
+    }
+    private void inicializar_datos() {
+
+        btnCalcular = findViewById(R.id.btnStartTravel);
+        btnSelectCity = findViewById(R.id.btnSelectCity);
+        LlBottom = findViewById(R.id.LlListCities);
+        LLPortada = findViewById(R.id.LLPortada);
+        lvCities = findViewById(R.id.lvCities);
+
+    }
+
 }
